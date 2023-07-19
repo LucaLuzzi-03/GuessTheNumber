32d9d2b644573d306f7f4bf6ee8f358fe683cbae
LucaLuzzi-03 <luzziluca6@gmail.com>
2023-07-19 10:50:56 -0300

    Proyecto finalizado

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = value => {
+  document.querySelector('.score').textContent = value;
+};
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //   Where there is no input
+  if (!guess) {
+    displayMessage('No number!');
+    displayScore((score -= 1));
+
+    // Where player wins
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      displayScore((score -= 1));
+    } else {
+      displayMessage('You lost the game!');
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  displayScore(score);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
